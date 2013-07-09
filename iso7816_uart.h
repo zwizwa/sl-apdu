a79dac7e1fdc27d0920177660f4b1c212ca70ccc
Tom Schouten <tom@zwizwa.be>
2013-07-09 12:20:05 -0400

    import iso7816_uart.* from openpcd:firmware/src/simtrace/

---
+++ iso7816_uart.h
@@ -0,0 +1,8 @@
+
+struct simtrace_stats *iso_uart_stats_get(void);
+void iso_uart_stats_dump(void);
+void iso_uart_dump(void);
+void iso_uart_rst(unsigned int state);
+void iso_uart_rx_mode(void);
+void iso_uart_clk_master(unsigned int master);
+void iso_uart_init(void);
